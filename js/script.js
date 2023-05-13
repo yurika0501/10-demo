6ba6543331d5eaf1dd2f2d7620d6a5517c4da04b
yurika0501 <titizuzu0501@gmail.com>
2023-05-13 03:29:51 +0000

    [Add]JSを効率化

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ const isAgreed = document.querySelector('#check');
 const btn = document.querySelector('#btn');
 
 isAgreed.addEventListener('change', () => {
-    // チェックボックスにチェックが入っているなら
-    if (isAgreed.checked){//「===true」は省略してもOK
-        // ボタンの無効化をなしにする
-        btn.disabled = false;
-        // そうじゃないなら（チェックが入っていないなら）
-    } else {
-        // ボタンを無効化する
-        btn.disabled = true;
-    }
+    // // チェックボックスにチェックが入っているなら
+    // if (isAgreed.checked){//「===true」は省略してもOK
+    //     // ボタンの無効化をなしにする
+    //     btn.disabled = false;
+    //     // そうじゃないなら（チェックが入っていないなら）
+    // } else {
+    //     // ボタンを無効化する
+    //     btn.disabled = true;
+    // }
+    
+    // 上記を省略した記述
+    btn.disabled = !isAgreed.checked;
 });
